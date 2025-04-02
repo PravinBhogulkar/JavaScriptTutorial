9c0297f1d7904ba8dce9bafd10bbf70b18dab987
PravinBhogulkar <pravin.p.bhogulkar@gmail.com>
2025-04-02 19:49:06 +0530

    Exercise of Lesson 1

---
+++ Exercises/Lesson1/exercise1.js
@@ -0,0 +1,35 @@
+// Use alert(...); to display 'Good Morning!' in apopup.
+alert('Good Morning!');
+
+// Display your name in a popup
+alert('Pravin');
+
+// Using math, aclculate 10+5 in the console
+10+5
+
+// Calculate 20-5 in the console
+20-5
+
+// Calculate 2+2-5 in the console
+2+2-5
+
+// Use document.body.innerHTML = ...; to display 'Good Morning!' on the web page
+document.body.innerHTML = 'Good Morning!';
+
+// Display your name on the web page
+document.body.innerHTML = 'Pravin'
+
+/**
+ * Challenge Exercises
+ */
+
+// You order a T-shirt for $10, socks for $8 and dinner plates for $20.
+// User JavaScript to calculate the total code of your order.
+10+8+20
+
+// Your bank account has $100, you spent $20 on lunch, $50 on dinner,
+// and earn $200 from your job. Calculate how much money you have.
+100=20-50+200
+
+// Use document.body.innerHTML = ...; to make the web page blank.
+document.body.innerHTML = '';
